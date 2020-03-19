f9e94ac52c463bb65e254830d2146388e51c64ab
zareth <zareth@Rose-Q325UA>
2020-03-19 12:58:48 -0700

    stumped - Jonathan Brown 3/19/2020

--- PD6Project/solarPV/views.py
+++ PD6Project/solarPV/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'solarPV/index.html')
+
+def register(request):
+    return render(request, 'solarPV/solarPV-registration.html')
+
+def login(request):
+    return render(request, 'solarPV/solarPV-login.html')
